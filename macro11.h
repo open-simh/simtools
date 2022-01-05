c311f6a175fd52da171795bc093762f4eba4c136
Olaf Seibert <rhialto@falu.nl>
2022-01-05 21:30:15 +0100

    Release version 0.7.2
    since the current state seems stable.

--- macro11.h
+++ macro11.h
@@ -3,12 +3,12 @@
 
 #include "git-info.h"
 
-#define BASE_VERSION "0.8wip"
+#define BASE_VERSION "0.7.2"
 
 #if defined(GIT_VERSION)
 #define VERSIONSTR BASE_VERSION" ("GIT_VERSION"\n\t"GIT_AUTHOR_DATE")"
 #else
-#define VERSIONSTR BASE_VERSION" (xx Xxx 2021)"
+#define VERSIONSTR BASE_VERSION" (05 Jan 2022)"
 /*#define VERSIONSTR "0.3 (April 21, 2009)" */
 /*#define VERSIONSTR "0.2   July 15, 2001"  */
 #endif
